2f0acbcd72663c448d50d932fb09ba8561900d8a
Gin154 <geran1_@hotmail.com>
2025-05-07 23:45:02 -0300

    Update 1.4.3
    Visual improvements
    -Fixed thumbnails resizing based on height
    
    Car information looks improved
    -Reordered car information and description
    -Added user information to car page
    -Improved compatibility with smaller screens

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secretsettings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = secretsettings.DEBUG
 
 ALLOWED_HOSTS = secretsettings.ALLOWED_HOSTS
 
